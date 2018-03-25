8ffab4d138c1fca5dd4ca6c09ed874f67c3931b4
段体华 <duantihua@gmail.com>
2018-03-25 11:54:24 +0800

    more upload

--- adminapp/src/main/scala/com/yushang/website/etls/web/action/manage/UploadJsonAction.scala
+++ adminapp/src/main/scala/com/yushang/website/etls/web/action/manage/UploadJsonAction.scala
@@ -1,7 +1,22 @@
 package com.yushang.website.etls.web.action.manage
 
 import org.beangle.webmvc.api.action.ActionSupport
+import org.beangle.commons.collection.Properties
+import org.beangle.webmvc.api.view.Stream
 
 class UploadJsonAction extends ActionSupport{
   
+   @mapping("{fileName}")
+   def index(@param("fileName") fileName:String):View={
+       var fileDir=SystemInfo.user.home+"/etls/upload/"+fileName
+       Stream(new File(fileDir+fileName))
+   }
+
+   def upload:Properties={
+       var fileDir=SystemInfo.user.home+"/etls/upload/"
+       //保存文件
+       val fileName="";
+
+	new Properties("error"->0 ,"url"-> "/upload-json/"+fileName)
+   }
 }
